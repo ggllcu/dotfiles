9c10ea0f05710a815ded677e7c65c6960aa599f2
Luca Guglielmi <87c21@live.it>
2023-09-06 12:53:07 +0200

    Adding some keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -28,7 +28,7 @@ require('lazy').setup({
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
+      -- Automatically install LSPs o stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
 
