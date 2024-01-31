331acc8498814231ae06522f3ed69a3d0e374658
Luca Guglielmi <87c21@live.it>
2024-01-31 11:51:06 +0100

    Moving to lazy file based config

---
+++ config/nvim/lua/lg/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true
+    })
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin-frappe]])
+    end,
+}
---
+++ config/nvim/lua/lg/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	'tpope/vim-sleuth',
+	'folke/which-key.nvim',
+	'RRethy/vim-illuminate',
+	'vim-test/vim-test',
+	'nvim-neorg/neorg',
+	'nvim-lua/plenary.nvim',
+	'j-hui/fidget.nvim',
+	'folke/neodev.nvim',
+}
