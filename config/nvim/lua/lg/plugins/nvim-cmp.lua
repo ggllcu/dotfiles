84a1b73b46c71b53a7c19b64392dfed15e60d537
Luca Guglielmi <87c21@live.it>
2024-01-31 15:09:07 +0100

    Fixing command line

--- config/nvim/lua/lg/plugins/nvim-cmp.lua
+++ config/nvim/lua/lg/plugins/nvim-cmp.lua
@@ -27,14 +27,14 @@ return {
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
@@ -64,13 +64,14 @@ return {
       }
     })
 
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = 'path' }
       }, {
-        { name = 'cmdline' }
-      })
+          { name = 'cmdline' }
+        })
     })
   end,
 }
