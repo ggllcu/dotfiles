3720326df7d449ae22d03ae3e36a379f5ac701ac
Luca Guglielmi <87c21@live.it>
2024-01-31 15:56:30 +0100

    Adding alacritty and home-manager symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,4 +1,6 @@
 ln -s ~/dotfiles/config/smug ~/.config/
 ln -s ~/dotfiles/config/nvim ~/.config/
-ln -s ~/dotfiles/config/tmux ~/.config/
+ln -s ~/dotfiles/config/alacritty ~/.config/
+ln -s ~/dotfiles/config/home-manager ~/.config/
+ln -s ~/dotfiles/config/nvim ~/.config/
 ln -s ~/dotfiles/vimrc ~/.vimrc
