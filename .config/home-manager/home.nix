d3cb0f6837c12f07cc230190e93f0db09c970899
Luca Guglielmi <87c21@live.it>
2024-02-18 14:26:01 +0100

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -16,8 +16,8 @@ in
 {
 # Home Manager needs a bit of information about you and the paths it should
 # manage.
-  home.username = "lucaguglielmi";
-  home.homeDirectory = "/home/lucaguglielmi";
+  home.username = "lg";
+  home.homeDirectory = "/home/lg";
 
 # This value determines the Home Manager release that your configuration is
 # compatible with. This helps avoid breakage when a new Home Manager release
