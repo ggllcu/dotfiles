c4189fc13f9d5397dc54fca624d685142725db13
Luca Guglielmi <luca.guglielmi@ext.athonet.com>
2024-06-24 14:42:04 +0200

    Fixing nix home manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -14,28 +14,28 @@ tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
 };
 in
 {
-  nixpkgs.config.allowUnfree = true;
-# Home Manager needs a bit of information about you and the paths it should
-# manage.
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
   home.username = "lg";
   home.homeDirectory = "/home/lg";
 
-# This value determines the Home Manager release that your configuration is
-# compatible with. This helps avoid breakage when a new Home Manager release
-# introduces backwards incompatible changes.
-#
-# You should not change this value, even if you update Home Manager. If you do
-# want to update the value, then make sure to first check the Home Manager
-# release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
-# The home.packages option allows you to install Nix packages into your
-# environment.
-    fonts.fontconfig.enable = true;
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
   home.packages = with pkgs; [
-    # warp-terminal
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
     firefox
-    postman
     chromium
     go-task
     btop
@@ -43,72 +43,74 @@ in
     clipboard-jh
     tldr
     kitty
-      stow
-      fzf
-      bat
-      eza
-      cargo
-      jq
-      gnomeExtensions.pano
-      gimp
-      monaspace
-      neovim
-      erlang_26
-      elixir_1_16
-      ripgrep
-      smug
-      inotify-tools
-      thefuck
-      zoxide
-      # nushell
-      # zsh
-      starship
+    stow
+    fzf
+    bat
+    eza
+    cargo
+    jq
+    gnomeExtensions.pano
+    gimp
+    monaspace
+    neovim
+    erlang_26
+    elixir_1_16
+    ripgrep
+    smug
+    inotify-tools
+    thefuck
+    zoxide
+    # nushell
+    # zsh
+    starship
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-# # It is sometimes useful to fine-tune packages, for example, by applying
-# # overrides. You can do that directly here, just don't forget the
-# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-# # fonts?
-      (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 
-# # You can also create simple shell scripts directly inside your
-# # configuration. For example, this adds a command 'my-hello' to your
-# # environment:
-# (writeShellScriptBin "my-hello" ''
-#   echo "Hello, ${config.home.username}!"
-# '')
-      ];
-
-# Home Manager is pretty good at managing dotfiles. The primary way to manage
-# plain files is through 'home.file'.
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
   home.file = {
-# # Building this configuration will create a copy of 'dotfiles/screenrc' in
-# # the Nix store. Activating the configuration will then make '~/.screenrc' a
-# # symlink to the Nix store copy.
-# ".screenrc".source = dotfiles/screenrc;
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
 
-# # You can also set the file content immediately.
-# ".gradle/gradle.properties".text = ''
-#   org.gradle.console=verbose
-#   org.gradle.daemon.idletimeout=3600000
-# '';
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
   };
 
-# Home Manager can also manage your environment variables through
-# 'home.sessionVariables'. If you don't want to manage your shell through Home
-# Manager then you have to manually source 'hm-session-vars.sh' located at
-# either
-#
-#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-#
-# or
-#
-#  /etc/profiles/per-user/lucaguglielmi/etc/profile.d/hm-session-vars.sh
-#
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/lg/etc/profile.d/hm-session-vars.sh
+  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-
-  programs.bash.enable = true;
 
   programs.fish = {
     enable = true;
@@ -151,6 +153,6 @@ in
       '';
   };
 
-# Let Home Manager install and manage itself.
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
