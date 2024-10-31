25a141aea1adf00fe852021b09ae4c8e9eb1642a
Luca Guglielmi <luca.guglielmi@ext.athonet.com>
2024-10-31 12:48:09 +0100

    Adding Fish

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,6 @@ end
 
 -- Appearance
 config.font_size = 14.0
-config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = false
