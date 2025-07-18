1a38243405875651375ff182a12fe4fd84a62120
Luca Guglielmi <luca.guglielmi@ext.athonet.com>
2025-07-18 12:24:38 +0200

    Adding lsp

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,6 @@
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "<leader>cf", function()
+    require("conform").format({
+        lsp_format = "fallback",
+    })
+end, { desc = "Format current file" })
